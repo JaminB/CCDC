1bca06ed48747ed58109cea00803a2bfb84aa705
Luke Snyder <lsnyde10@uncc.edu>
2014-01-30 13:25:32 -0500

    Added script to check crontab

---
+++ checkCrontab.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+## Checks every user on the system for a crontab. If there is a crontab, print the username and the contents, minus comments.
+
+for user in `cat /etc/passwd | cut -d ":" -f 1`; do
+        cron=$(sudo -u $user crontab -l 2> /dev/null| grep -v "#")
+        if [ "$cron" ]; then
+                echo "$user"
+                echo "$cron"
+        fi
+done
